bfb0fc630c5fe073577c5eda5767998b8e452c39
maroof <maroof@noonacademy.com>
2020-05-27 14:38:49 +0530

    scss

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,39 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index'),
+  entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  resolve: {
+    modules: [__dirname, 'src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts', '.css'],
+  },
   module: {
-    rules: [{
-      test: /\.js$/,
-      include: path.resolve(__dirname, 'src'),
-      use: ['babel-loader']
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      }
+    ]
   },
   devServer: {
-    contentBase:  path.resolve(__dirname, 'dist'),
-    port: 9000
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 3000
   },
   plugins: [
     new HtmlWebpackPlugin({
